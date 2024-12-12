74216aed10fe02a9a6d7b31ded5d72dfadb57c7d
Kay Cha <kay@hannal.net>
2024-12-12 22:51:29 +0900

    ADD 계정 ID로 사용자 정보 가져오는 API와 테스트코드

--- tests/apps/account/test_endpoints.py
+++ tests/apps/account/test_endpoints.py
@@ -1,13 +1,11 @@
 import pytest
 
+from sqlmodel import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException, status
 from fastapi.testclient import TestClient
-from appserver.app import app
 from appserver.apps.account.endpoints import user_detail
-from appserver.db import create_async_engine, create_session
 from appserver.apps.account.models import User
-from appserver.apps.calendar.models import Calendar
-from sqlmodel import SQLModel
 
 
 async def test_user_detail_successfully():
@@ -27,9 +25,7 @@ async def test_user_detail_not_found():
     assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
 
 
-def test_user_detail_by_http():
-    client = TestClient(app)
-
+def test_user_detail_by_http(client: TestClient):
     response = client.get("/account/users/test")
 
     assert response.status_code == status.HTTP_200_OK
@@ -43,49 +39,30 @@ def test_user_detail_by_http():
     assert data["updated_at"] is not None
 
 
-async def test_user_detail_by_http_not_found():
-    client = TestClient(app)
-
+async def test_user_detail_by_http_not_found(client: TestClient):
     response = client.get("/account/users/not_found")
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-# dsn = "sqlite+aiosqlite:///:memory:"
-dsn = "sqlite+aiosqlite:///./test.db"
-engine = create_async_engine(dsn)
-
-async def test_user_detail_for_real_user():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
-
-    session_factory = create_session(engine)
-
-    async with session_factory() as session:
-        user = User(
-            username="test",
-            password="test",
-            email="test@example.com",
-            display_name="test",
-            is_host=True,
-        )
-        session.add(user)
-        await session.commit()
-
-    client = TestClient(app)
+async def test_user_detail_for_real_user(client: TestClient, db_session: AsyncSession):
+    user = User(
+        username="test",
+        password="test",
+        email="test@example.com",
+        display_name="test",
+        is_host=True,
+    )
+    db_session.add(user)
+    await db_session.commit()
+    await db_session.flush()
 
     response = client.get(f"/account/users/{user.username}")
-
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["username"] == "test"
-    assert data["email"] == "test@example.com"
-    assert data["display_name"] == "test"
+    assert data["username"] == user.username
+    assert data["email"] == user.email
+    assert data["display_name"] == user.display_name
 
     response = client.get("/account/users/not_found")
     assert response.status_code == status.HTTP_404_NOT_FOUND
-
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
-    await engine.dispose()
