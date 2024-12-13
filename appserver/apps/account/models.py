0ac48c96142116fa116c81c91d5d13d99cf186df
Kay Cha <kay@hannal.net>
2024-12-13 13:17:40 +0900

    ADD 회원가입 함수와 테스트 코드

--- appserver/apps/account/models.py
+++ appserver/apps/account/models.py
@@ -1,6 +1,8 @@
-from typing import TYPE_CHECKING, Union
+import random
+import string
+from typing import TYPE_CHECKING, Self, Union
 from datetime import datetime, timezone
-from pydantic import AwareDatetime, EmailStr
+from pydantic import AwareDatetime, EmailStr, model_validator
 from sqlalchemy import UniqueConstraint
 from sqlmodel import SQLModel, Field, Relationship, func
 from sqlalchemy_utc import UtcDateTime
@@ -16,10 +18,10 @@ class User(SQLModel, table=True):
     )
 
     id: int = Field(default=None, primary_key=True)
-    username: str = Field(max_length=40, description="사용자 계정 ID")
+    username: str = Field(min_length=4, max_length=40, description="사용자 계정 ID")
     email: EmailStr = Field(unique=True, max_length=128, description="사용자 이메일")
-    display_name: str = Field(max_length=40, description="사용자 표시 이름")
-    password: str = Field(max_length=128, description="사용자 비밀번호")
+    display_name: str = Field(min_length=4, max_length=40, description="사용자 표시 이름")
+    password: str = Field(min_length=8, max_length=128, description="사용자 비밀번호")
     is_host: bool = Field(default=False, description="사용자가 호스트인지 여부")
 
     oauth_accounts: list["OAuthAccount"] = Relationship(back_populates="user")
@@ -46,6 +48,13 @@ class User(SQLModel, table=True):
             "onupdate": lambda: datetime.now(timezone.utc),
         },
     )
+
+    @model_validator(mode="before")
+    @classmethod
+    def generate_display_name(cls, data: dict) -> dict:
+        if not data.get("display_name"):
+            data["display_name"] = "".join(random.choices(string.ascii_letters + string.digits, k=8))
+        return data
 
 
 class OAuthAccount(SQLModel, table=True):
