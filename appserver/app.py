099f50a00024c9d2a65c0f63c1dab62e337beac2
Kay Cha <kay@hannal.net>
2024-12-23 15:43:26 +0900

    ADD User 모델에 대한 UserAdmin 정의

--- appserver/app.py
+++ appserver/app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from .apps.account.endpoints import router as account_router
-from .apps.calendar.endpoints import router as calendar_router
+from sqladmin import Admin
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from appserver.apps.account.endpoints import router as account_router
+from appserver.apps.calendar.endpoints import router as calendar_router
+from appserver.admin import include_admin_views
+from .db import engine
 
 app = FastAPI()
 
@@ -9,3 +14,11 @@ def include_routers(_app: FastAPI):
     _app.include_router(calendar_router)
 
 include_routers(app)
+
+
+def init_admin(_app: FastAPI, _engine: AsyncEngine):
+    return Admin(_app, _engine)
+
+
+admin = init_admin(app, engine)
+include_admin_views(admin)
