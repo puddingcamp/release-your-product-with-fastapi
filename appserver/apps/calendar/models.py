4f8004647efb708b132469abf64abe28aa01dadd
Kay Cha <kay@hannal.net>
2025-07-21 16:27:35 +0900

    Add Booking.files에 lazy 설정.

--- appserver/apps/calendar/models.py
+++ appserver/apps/calendar/models.py
@@ -133,7 +133,7 @@ class Booking(SQLModel, table=True):
 
     files: list["BookingFile"] = Relationship(
         back_populates="booking",
-        sa_relationship_kwargs={"lazy": "noload"},
+        sa_relationship_kwargs={"lazy": "joined"},
     )
 
     google_event_id: str | None = Field(
